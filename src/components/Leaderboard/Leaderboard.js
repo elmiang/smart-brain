593af4df524cb0c5841a729f4e4fc723af80aad9
elmiang <eldermiang@gmail.com>
2023-08-01 19:42:46 +1000

    Add leaderboard toggling functionality (entries/facecount)

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './Leaderboard.css';
 import axios from "axios";
 
@@ -7,26 +7,34 @@ const faceRankings = await axios.get(`${process.env.REACT_APP_SERVER_URL}/rankin
 
 let rankingsData = entryRankings;
 
-// if (rankingType === "entries") {
-//   rankingsData = entryRankings;
-// }
-// else {
-//   rankingsData = faceRankings
-// }
-
-const rankings = rankingsData.data.map((user, index) => {
-  const { name, entries, faces } = user; 
-  return (
-    <tr>
-      <td>{index + 1}</td>
-      <td>{name}</td>
-      <td>{entries}</td>
-      <td>{faces}</td>
-    </tr>
-  )
-});
-
 const Leaderboard = ({ loggedUser }) => {
+  const [entriesSelected, setEntriesSelection] = useState(true);
+
+  //Update rankings data source based on the filter selected
+  useEffect(() => {
+    //Inverted statement is giving the correct results as opposed to the expected statement
+    //Something going on with react async state changes and rendering interaction?
+    if (!entriesSelected) {
+      rankingsData = entryRankings;
+    }
+    else {
+      rankingsData = faceRankings;
+    }
+  }, [entriesSelected]);
+
+  let rankings = rankingsData.data.map((user, index) => {
+    const { name, entries, faces } = user; 
+    return (
+      <tr>
+        <td>{index + 1}</td>
+        <td>{name}</td>
+        <td>{entries}</td>
+        <td>{faces}</td>
+      </tr>
+    )
+  });
+
+  //Ranking data of the currently logged in user
   let currentRanking;
   if (loggedUser.id) {
     let currentUser = rankingsData.data.filter(user => user.name === loggedUser.name)[0];
@@ -49,8 +57,10 @@ const Leaderboard = ({ loggedUser }) => {
   return(
     <div className="flex flex-column items-center">
       <div className="controls">
-        <a className="button1">Entries</a>
-        <a className="button2">Face Count</a>
+        <input type="radio" id="entries" value="Entries" name="sortingType" defaultChecked={false} onClick={() => setEntriesSelection(true)}/>
+        <label htmlFor="entries">Entries</label>
+        <input type="radio" id="faces" value="Faces" name="sortingType" onClick={() => setEntriesSelection(false)}/>
+        <label htmlFor="faces">Faces</label>
       </div>
       <table id="leaderboard">
         <thead>
